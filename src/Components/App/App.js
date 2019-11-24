8c7b93d793d504414345bae330f3ffbe928ab966
AnnaP <ana.sachova@gmail.com>
2019-11-24 20:33:40 +0200

    Delete unnecessary commented methods

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -22,39 +22,9 @@ function App() {
     const routeResult = useRoutes(routes);
     const [appState, setAppState] = useContext(AppContext);
 
-    // useEffect(() => {
-    //     console.log("useEffect1");
-    //     redirect();
-    //     console.log("useEffect2");
-    // }, [appState.actions, appState.loggedIn]);
-
-
-    // useEffect(() => {
-    //      appState.actions.updatePosts();
-    // },[appState.actions]);
-
-    // useEffect(() => {
-    //     if (!appState.actions.checkLoginState()) {
-    //         appState.actions.login('suprem1@mail.ru', 'String1');
-    //     }
-    //
-    // }, [appState.actions]);
-
-    useEffect(() => {
-        appState.actions.checkLoginState();
-    }, [appState.actions]);
-
-
-    // useEffect(() => {
-    //     if (appState.navigationItem === 'home') {
-    //         navigate('/search-field');
-    //         console.log("navigate from useEffect");
-    //     }
-    // }, [appState.actions, appState.navigationItem, appState.loggedIn]);
-
     useEffect(() => {
         showAuthPage();
-        console.log("navigate from useEffect");
+        console.log("navigate from useEffect 'showAuthPage()' ");
     }, [appState.actions, appState.loggedIn]);
 
 
@@ -67,23 +37,12 @@ function App() {
         appState.actions.getAdvertsVersion2(appState.advertsByCity, appState.advertsBySalary, appState.advertsByTechnology);
     }, [appState.actions, appState.advertsByCity, appState.advertsBySalary, appState.advertsByTechnology]);
 
-
-    //  useEffect(() => {
-    //     appState.actions.getAdverts(appState.searchBy, appState.advertsCriteria, appState.advertsTechnology);
-    // }, [appState.actions, appState.searchBy, appState.advertsCriteria, appState.advertsTechnology]);
-
-
-    //const redirect = useRedirect('/login', '/search-field');
-
-
     const showAuthPage = () => {
         if (appState.loggedIn) {
             console.log("message from AuthPage OK");
             navigate('/');
             setAppState(state => ({...state, navigationItem: 'home'}));
             console.log("navigationItem " + appState.navigationItem);
-
-            // return (<Redirect to={{pathname: '/search-field'}}/>);
         } else {
             console.log("message from AuthPage NOT OK");
             return <h3>User not loggedIn</h3>;
@@ -99,43 +58,13 @@ function App() {
         if (name === 'logout') {
             appState.actions.logout();
         }
-
     };
 
     return (
         <div className="App">
 
-
-            {/*<BrowserRouter>*/}
-
-
-            {/*    <Container>*/}
-
-            {/*        <NavBar/>*/}
-            {/*        {appState.loggedIn ? "Logged in" : "Not logged in"}*/}
-            {/*        /!*{appState.navigationItem === 'login' && <LoginForm/>}*!/*/}
-            {/*        /!*{appState.navigationItem === 'home' && <SearchField/>}*!/*/}
-            {/*        <Grid centered id="searchElement">*/}
-            {/*            /!*<SearchField/>*!/*/}
-            {/*            /!*{appState.criteria === 'Kaunas' && <SecretComponent/>}*!/*/}
-            {/*            /!*{appState.adverts.length > 0 && <AdvertList/>}*!/*/}
-            {/*        </Grid>*/}
-            {/*        <Switch>*/}
-            {/*            /!*<Redirect to={isLoggedIn ? "/wallet/portfolio" : "/wallet/login"}/>*!/*/}
-            {/*            /!*<Redirect exact from='/' to='/search-field'/>*!/*/}
-            {/*            <Route exact path="/search-field" component={SearchField}/>*/}
-            {/*            <Route path="/login" component={LoginForm}/>*/}
-            {/*            <Route component={NotFound}/>*/}
-            {/*        </Switch>*/}
-
-            {/*    </Container>*/}
-            {/*</BrowserRouter>*/}
-
-
             <Container>
                 <Menu>
-                    {/*     <Menu.Item as={Link} to='/search-field' name='home'*/}
-                    {/*active={appState.navigationItem === 'home'}>Home</Menu.Item>*/}
 
                     <Menu.Item name='home' active={appState.navigationItem === 'home'} onClick={handleItemClick}>
                         <A href="/">Home Page</A>
@@ -177,9 +106,6 @@ function App() {
 
 export default App;
 
-//TODO convert in backend Set<Technology> to Set<String>
+
 //TODO make rows of adverts clickable (add link to whole row)
 //TODO pagination
-
-
-//TODO BUG redirect after login
--- src/Components/Signup/SignUpForm.js
+++ src/Components/Signup/SignUpForm.js
@@ -8,7 +8,6 @@ export default function SignUpForm() {
 
     const [inputs, setInputs] = useState({});
 
-
     const handleInputChange = (event) => {
         event.persist();
         setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
